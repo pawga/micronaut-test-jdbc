532375c126637cf1bf5925ada7b04c2bc327cf5e
Sergey Ivannikov <ivannikov.s@gmail.com>
2023-12-20 22:39:31 +0300

    contayners 3

--- src/test/kotlin/com/pawga/DictionaryRepositoryWithoutUrlTest.kt
+++ src/test/kotlin/com/pawga/DictionaryRepositoryWithoutUrlTest.kt
@@ -1,9 +1,10 @@
 package com.pawga
 
-import io.micronaut.context.annotation.Property
+import io.micronaut.core.annotation.NonNull
 import io.micronaut.core.io.ResourceLoader
 import io.micronaut.test.annotation.Sql
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest
+import io.micronaut.test.support.TestPropertyProvider
 import jakarta.inject.Inject
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.count
@@ -11,25 +12,40 @@ import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestInstance
 import org.slf4j.LoggerFactory
+import org.testcontainers.containers.PostgreSQLContainer
+import org.testcontainers.junit.jupiter.Container
+import org.testcontainers.junit.jupiter.Testcontainers
+import org.testcontainers.utility.MountableFile
 import java.sql.Connection
+import java.util.Map
 
-@MicronautTest(startApplication = false)
-@Property(
-    name = "datasources.default.driver-class-name",
-    value = "org.testcontainers.jdbc.ContainerDatabaseDriver"
-)
-@Property(
-    name = "datasources.default.url",
-    value = "jdbc:tc:postgresql:15.2-alpine:///db?TC_INITSCRIPT=sql/init-db-2.sql"
-)
+@MicronautTest(startApplication = false) // <1>
+@Testcontainers(disabledWithoutDocker = true) // <2>
+@TestInstance(TestInstance.Lifecycle.PER_CLASS) // <3>
 @Sql(scripts = ["classpath:sql/seed-data-2.sql"], phase = Sql.Phase.BEFORE_EACH)
-class DictionaryRepositoryWithJdbcUrlTest {
+internal class DictionaryRepositoryWithoutUrlTest : TestPropertyProvider {
+    override fun getProperties(): @NonNull MutableMap<String, String>? { // <5>
+        if (!postgres.isRunning) {
+            postgres.start()
+        }
+        return Map.of(
+            "datasources.default.driver-class-name", "org.postgresql.Driver",
+            "datasources.default.url", postgres.jdbcUrl,
+            "datasources.default.username", postgres.username,
+            "datasources.default.password", postgres.password
+        )
+    }
+
     @Inject
     var connection: Connection? = null
 
     @Inject
     var resourceLoader: ResourceLoader? = null
+
+    @Inject
+    var dictionaryRepository: DictionaryRepository? = null
 
     @Test
     fun `test Repository`(dictionaryRepository: DictionaryRepository) {
@@ -75,7 +91,15 @@ class DictionaryRepositoryWithJdbcUrlTest {
 
     companion object {
         private val log = LoggerFactory.getLogger(DictionaryRepositoryWithJdbcUrlTest::class.java)
-        const val NEW_NAME = "Test value"
-        const val UPDATE_NAME = "Changed value"
+        private const val NEW_NAME = "Test value"
+        private const val UPDATE_NAME = "Changed value"
+
+        @Container
+        private val postgres: PostgreSQLContainer<*> = PostgreSQLContainer(
+            "postgres:15.2-alpine"
+        ).withCopyFileToContainer(
+            MountableFile.forClasspathResource("sql/init-db-2.sql"),
+            "/docker-entrypoint-initdb.d/init-db.sql"
+        )
     }
 }
