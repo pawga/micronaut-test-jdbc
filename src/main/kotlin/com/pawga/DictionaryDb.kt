5c386d67729398c31fcfd4fe041b9e3cee7c38f5
Sergey Ivannikov <ivannikov.s@gmail.com>
2023-12-20 15:00:24 +0300

    dictionary

---
+++ src/main/kotlin/com/pawga/DictionaryDb.kt
@@ -0,0 +1,29 @@
+package com.pawga
+
+import io.micronaut.data.annotation.GeneratedValue
+import io.micronaut.data.annotation.Id
+import io.micronaut.data.annotation.MappedEntity
+import io.micronaut.data.annotation.Relation
+import io.micronaut.data.annotation.Relation.Cascade
+import io.micronaut.serde.annotation.Serdeable
+
+/**
+ * Created by sivannikov
+ */
+
+//@Serdeable
+//@MappedEntity(value = "dictionary")
+//data class DictionaryDb(
+//    @Id
+////    @GeneratedValue
+//    val id: Long? = null,
+//    val name: String,
+//)
+
+@MappedEntity("dictionary")
+data class DictionaryDb(
+    @Id // <2>
+    val id: Long,
+    val name: String
+)
+
---
+++ src/main/kotlin/com/pawga/DictionaryRepository.kt
@@ -0,0 +1,28 @@
+package com.pawga
+
+import io.micronaut.data.exceptions.DataAccessException
+import io.micronaut.data.jdbc.annotation.JdbcRepository
+import io.micronaut.data.model.query.builder.sql.Dialect
+import io.micronaut.data.repository.jpa.kotlin.CoroutineJpaSpecificationExecutor
+import io.micronaut.data.repository.kotlin.CoroutineCrudRepository
+import jakarta.transaction.Transactional
+
+/**
+ * Created by sivannikov on 11.12.2023 13:18
+ */
+
+@JdbcRepository(dialect = Dialect.POSTGRES)
+abstract class DictionaryRepository :
+    CoroutineCrudRepository<DictionaryDb, Long> {
+//    abstract fun save(
+//        dictionary: DictionaryDb,
+//    ): DictionaryDb
+//
+//    @Transactional
+//    open fun saveWithException(
+//        dictionary: DictionaryDb,
+//    ): DictionaryDb {
+//        save(dictionary)
+//        throw DataAccessException("test exception")
+//    }
+}
